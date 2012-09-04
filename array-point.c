e2ae71aeae1609ae1393d51bfcb2778f5b4ba81e
tingxueren <zhangdongsheng1224@gmail.com>
2012-09-04 16:30:32 +0800

    the difficult of array and pointer

---
+++ array-point.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+char ga[] = "abcdefghijklm";
+
+void my_array_func(char ca[10])
+{
+	printf("addr of array param = %#x\n", &ca);
+	printf("addr (ca[0]) = %#x\n", &(ca[0]));
+	printf("addr (ca[1]) = %#x\n", &(ca[1]));
+	printf("++ca = %#x \n\n", ++ca);
+	printf("*(ca++) = %c \n\n", *(ca++));
+	printf("*(++ca) = %c \n\n", *(++ca));
+}	
+
+void my_pointer_func(char *pa)
+{
+	printf("addr of ptr param = %#x\n", &pa);
+	printf("addr (pa[0]) = %#x\n", &(pa[0]));
+	printf("addr (pa[1]) = %#x\n", &(pa[1]));
+	printf("++pa = %#x \n\n", ++pa);
+}	
+
+void main()
+{
+	printf("addr of global param = %#x\n", &ga);
+	printf("addr (ga[0]) = %#x\n", &(ga[0]));
+	printf("addr (ga[1]) = %#x\n\n", &(ga[1]));
+	my_array_func(ga);
+	my_pointer_func(ga);
+	
+}	
